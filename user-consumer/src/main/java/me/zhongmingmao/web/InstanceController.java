fde840b9afc243cac9c42fc67165cc059ed0f856
zhongmingmao <zhongmingmao0625@gmail.com>
2017-06-15 13:50:15 +0800

    Close #10 Modify Ribbon Config In <User Consumer>

--- user-consumer/src/main/java/me/zhongmingmao/web/InstanceController.java
+++ user-consumer/src/main/java/me/zhongmingmao/web/InstanceController.java
@@ -3,6 +3,7 @@ package me.zhongmingmao.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,14 +11,24 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("instances")
+@RequestMapping("/instances")
 public class InstanceController {
+    
+    private static final String USER_SERVICE_ID = "user-provider";
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
     
     @Autowired
     private DiscoveryClient discoveryClient;
     
-    @GetMapping("/user-provider")
+    @GetMapping("/user-provider-list")
     public List<ServiceInstance> userProviders() {
-        return discoveryClient.getInstances("user-provider");
+        return discoveryClient.getInstances(USER_SERVICE_ID);
+    }
+    
+    @GetMapping("/current-user-provider")
+    public String currentUserProvider() {
+        ServiceInstance instance = loadBalancerClient.choose(USER_SERVICE_ID);
+        return String.format("%s@%s:%s", instance.getServiceId(), instance.getHost(), instance.getPort());
     }
 }
--- user-provider/src/main/java/me/zhongmingmao/web/UserController.java
+++ user-provider/src/main/java/me/zhongmingmao/web/UserController.java
@@ -1,8 +1,8 @@
 package me.zhongmingmao.web;
 
-import me.zhongmingmao.domain.User;
 import me.zhongmingmao.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,11 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    
+    @Value("#{environment['spring.cloud.client.hostname']}") // SPEL
+    private String hostname;
+    
     @Autowired
     private UserRepository userRepository;
     
     @GetMapping("/{id}")
-    public User findById(@PathVariable Long id) {
-        return userRepository.findOne(id);
+    public String findById(@PathVariable Long id) {
+        return String.format("[%s response %s", hostname, userRepository.findOne(id));
     }
 }
