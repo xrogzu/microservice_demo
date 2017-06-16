de25dc0899abb11c2d5408da4a744d9fb8833f9b
zhongmingmao <zhongmingmao0625@gmail.com>
2017-06-16 14:50:57 +0800

    Close #15 Integrated Hystrix With Robbin In <User Consumer>

--- user-consumer/src/main/java/me/zhongmingmao/UserConsumerApplication.java
+++ user-consumer/src/main/java/me/zhongmingmao/UserConsumerApplication.java
@@ -1,16 +1,14 @@
 package me.zhongmingmao;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
-@SpringBootApplication
-@EnableDiscoveryClient
+@SpringCloudApplication
 @EnableFeignClients
 public class UserConsumerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(UserConsumerApplication.class, args);
-	}
+    
+    public static void main(String[] args) {
+        SpringApplication.run(UserConsumerApplication.class, args);
+    }
 }
--- user-consumer/src/main/java/me/zhongmingmao/web/RibbonConsumerController.java
+++ user-consumer/src/main/java/me/zhongmingmao/web/RibbonConsumerController.java
@@ -1,6 +1,8 @@
 package me.zhongmingmao.web;
 
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,8 +17,20 @@ public class RibbonConsumerController {
     @Autowired
     private RestTemplate restTemplate;
     
+    @HystrixCommand(fallbackMethod = "findByIdFallback",
+            commandProperties = {
+                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000"),
+            },
+            threadPoolProperties = {
+                    @HystrixProperty(name = "coreSize", value = "1"),
+                    @HystrixProperty(name = "maxQueueSize", value = "10")})
     @GetMapping("/{id}")
     public String findById(@PathVariable Long id) {
         return restTemplate.getForObject(String.format("http://USER-PROVIDER/users/%s", id), String.class);
+    }
+    
+    
+    public String findByIdFallback(Long id) {
+        return String.format("findByIdFallback(%s) Error", id);
     }
 }
