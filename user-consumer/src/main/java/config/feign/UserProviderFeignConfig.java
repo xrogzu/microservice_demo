36058b2b3b7841c3c110ded906696f4f6b25b34d
zhongmingmao <zhongmingmao0625@gmail.com>
2017-06-16 10:31:28 +0800

    Close #13 Config Fegin Logger Level In <User Consumer>

---
+++ user-consumer/src/main/java/config/feign/UserProviderFeignConfig.java
@@ -0,0 +1,15 @@
+package config.feign;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class UserProviderFeignConfig {
+    
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
+    
+}
--- user-consumer/src/main/java/me/zhongmingmao/feign/UserFeignClient.java
+++ user-consumer/src/main/java/me/zhongmingmao/feign/UserFeignClient.java
@@ -1,5 +1,6 @@
 package me.zhongmingmao.feign;
 
+import config.feign.UserProviderFeignConfig;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 // FeignClient用于创建一个 Ribbon 负载均衡器
 // 当使用Eureka时,Ribbon会把name解析成Eureka Server上的服务
 // 否则解析成service.ribbon.listOfServers
-@FeignClient(name = "user-provider")
+@FeignClient(name = "user-provider", configuration = UserProviderFeignConfig.class)
 public interface UserFeignClient {
     
     @GetMapping("/users/{id}")
